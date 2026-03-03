b294f469fd587136e0970f883d2ff3567252765c
Hyukjoo-Lee <hjjaydon.lee@gmail.com>
2026-03-04 01:18:45 +0900

    서버 초기 구축

---
+++ invitation-backend/src/main/java/com/connectyou/invitation_backend/InvitationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connectyou.invitation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvitationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvitationBackendApplication.class, args);
+	}
+
+}
---
+++ invitation-backend/src/test/java/com/connectyou/invitation_backend/InvitationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectyou.invitation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvitationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
